63807303c96a88e2c980b5a77bed49ed9b8a2de3
keyle <keyle@capsule.org>
2025-01-05 14:12:22 +1000

    minor misunderstanding about const and cpp

--- comp/debug.h
+++ comp/debug.h
@@ -4,10 +4,10 @@
 
 #include <string>
 
-const std::string ANSI_COLOR_RED = "\033[31m";
-const std::string ANSI_COLOR_GREEN = "\033[32m";
-const std::string ANSI_COLOR_YELLOW = "\033[33m";
-const std::string ANSI_COLOR_BLUE = "\033[34m";
-const std::string ANSI_COLOR_RESET = "\033[0m";
+constexpr std::string ANSI_COLOR_RED = "\033[31m";
+constexpr std::string ANSI_COLOR_GREEN = "\033[32m";
+constexpr std::string ANSI_COLOR_YELLOW = "\033[33m";
+constexpr std::string ANSI_COLOR_BLUE = "\033[34m";
+constexpr std::string ANSI_COLOR_RESET = "\033[0m";
 
 void print_token(Token& token);
